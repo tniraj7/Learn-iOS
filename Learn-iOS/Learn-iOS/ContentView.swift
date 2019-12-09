d7cd1e4b42e5c1b111476322fc8aef7807aaab0c
Niraj Tiwari <tniraj7@gmail.com>
2019-12-09 15:08:04 +0900

    extract subviews and use them by applying modifers

--- Learn-iOS/Learn-iOS/ContentView.swift
+++ Learn-iOS/Learn-iOS/ContentView.swift
@@ -3,41 +3,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            VStack {
-                Text("Card Back")
-            }
-            .frame(width: 300.0, height: 220.0)
-            .background(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/)
-            .cornerRadius(10)
-            .shadow(radius: 20)
-            .offset(x: 0, y: -20)
+            CardView()
+                .offset(x: 0, y: -40)
+                .scaleEffect(0.85)
+                .rotationEffect(Angle(degrees: 15))
+                .rotation3DEffect(Angle(degrees: 50), axis: (x: 10.0, y: 10.0, z: 10.0))
+                .blendMode(.hardLight)
             
-            VStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("UI Design")
-                            .font(.headline)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color("accent"))
-                            .padding(.top)
-                        
-                        Text("Certificate")
-                            .foregroundColor(Color.white)
-                    }
-                    .background(Color.black)
-                    Spacer()
-                    Image("Logo")
-                        .resizable()
-                        .frame(width: 30.0, height: 30.0)
-                }
-                .padding(.horizontal)
-                Spacer()
-                Image("Background")
-            }
-            .frame(width: 340.0, height: 220.0)
-            .background(Color.black)
-            .cornerRadius(10)
-            .shadow(radius: 20)
+            CardView()
+                .offset(x: 0, y: -20)
+                .scaleEffect(0.9)
+                .rotationEffect(Angle(degrees: 10))
+                .rotation3DEffect(Angle(degrees: 40), axis: (x: 10.0, y: 10.0, z: 10.0))
+                .blendMode(.hardLight)
+            
+            CertificateView()
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: 5))
+                .rotation3DEffect(Angle(degrees: 30), axis: (x: 10.0, y: 10.0, z: 10.0))
+
+
         }
     }
 }
@@ -45,5 +30,48 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            Text("Card Back")
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/)
+        .cornerRadius(10)
+        .shadow(radius: 20)
+    }
+}
+
+struct CertificateView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("accent"))
+                        .padding(.top)
+                    
+                    Text("Certificate")
+                        .foregroundColor(Color.white)
+                }
+                .background(Color.black)
+                Spacer()
+                Image("Logo")
+                    .resizable()
+                    .frame(width: 30.0, height: 30.0)
+            }
+            .padding(.horizontal)
+            Spacer()
+            Image("Background")
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(10)
+        .shadow(radius: 20)
     }
 }
