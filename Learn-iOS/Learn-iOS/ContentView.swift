1f8d2d7bd6d574fb10c7bda7f5016d8b8bd0069c
Niraj Tiwari <tniraj7@gmail.com>
2019-12-09 13:30:22 +0900

    add base card in depth stack

--- Learn-iOS/Learn-iOS/ContentView.swift
+++ Learn-iOS/Learn-iOS/ContentView.swift
@@ -2,32 +2,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("UI Design")
-                        .font(.headline)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color("accent"))
-                        .padding(.top)
-                    
-                    Text("Certificate")
-                        .foregroundColor(Color.white)
-                }
-                .background(Color.black)
-                Spacer()
-                Image("Logo")
-                    .resizable()
-                    .frame(width: 30.0, height: 30.0)
+        ZStack {
+            VStack {
+                Text("Card Back")
             }
-            .padding(.horizontal)
-            Spacer()
-            Image("Background")
+            .frame(width: 300.0, height: 220.0)
+            .background(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/)
+            .cornerRadius(10)
+            .shadow(radius: 20)
+            .offset(x: 0, y: -20)
+            
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.headline)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("accent"))
+                            .padding(.top)
+                        
+                        Text("Certificate")
+                            .foregroundColor(Color.white)
+                    }
+                    .background(Color.black)
+                    Spacer()
+                    Image("Logo")
+                        .resizable()
+                        .frame(width: 30.0, height: 30.0)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Image("Background")
+            }
+            .frame(width: 340.0, height: 220.0)
+            .background(Color.black)
+            .cornerRadius(10)
+            .shadow(radius: 20)
         }
-        .frame(width: 340.0, height: 220.0)
-        .background(Color.black)
-        .cornerRadius(10)
-        .shadow(radius: 20)
     }
 }
 
