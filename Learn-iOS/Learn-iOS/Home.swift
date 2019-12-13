e48eabde1dde09ec6ead023531e643256570c12f
Niraj Tiwari <tniraj7@gmail.com>
2019-12-13 15:42:06 +0900

    extract subview and use binding variable to show it

--- Learn-iOS/Learn-iOS/Home.swift
+++ Learn-iOS/Learn-iOS/Home.swift
@@ -14,7 +14,6 @@ let menuData = [
 ]
 
 struct Home: View {
-    var menu = menuData
     @State var show = false
     
     var body: some View {
@@ -24,26 +23,7 @@ struct Home: View {
             }) {
                 Text("Open Menu")
             }
-
-            VStack(alignment: .leading, spacing: 20) {
-                ForEach(menu) { item in
-                    MenuRow(image: item.icon, text: item.title)
-                }
-                Spacer()
-            }
-            .padding(.top, 20)
-            .padding(30)
-            .frame(minWidth: 0, maxWidth: .infinity)
-            .background(Color.white)
-            .cornerRadius(30)
-            .padding(.trailing, 60)
-            .shadow(radius: 20)
-            .rotation3DEffect(Angle(degrees: show ? 0 : 60), axis: (x: 0, y: 10.0, z: 0))
-            .animation(.default)
-            .offset(x: show ? 0 : -UIScreen.main.bounds.width)
-            .onTapGesture {
-                self.show.toggle()
-            }
+            MenuView(show: $show)
         }
 
     }
@@ -72,3 +52,30 @@ struct MenuRow: View {
     }
 }
 
+
+struct MenuView: View {
+    var menu = menuData
+    @Binding var show: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ForEach(menu) { item in
+                MenuRow(image: item.icon, text: item.title)
+            }
+            Spacer()
+        }
+        .padding(.top, 20)
+        .padding(30)
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .padding(.trailing, 60)
+        .shadow(radius: 20)
+        .rotation3DEffect(Angle(degrees: show ? 0 : 60), axis: (x: 0, y: 10.0, z: 0))
+        .animation(.default)
+        .offset(x: show ? 0 : -UIScreen.main.bounds.width)
+        .onTapGesture {
+            self.show.toggle()
+        }
+    }
+}
