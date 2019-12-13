a80938144658ff3943cc442dc4a0016cda2b3365
Niraj Tiwari <tniraj7@gmail.com>
2019-12-13 19:20:07 +0900

    adjust height of CardBottomView

--- Learn-iOS/Learn-iOS/ContentView.swift
+++ Learn-iOS/Learn-iOS/ContentView.swift
@@ -143,6 +143,6 @@ struct CardBottomView: View {
         .background(Color.white)
         .cornerRadius(30)
         .shadow(radius: 20)
-        .offset(y: 720)
+        .offset(y: 600)
     }
 }
