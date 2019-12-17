c0c8e85de27c6dc15abc4a1378cb86bfe3173268
Niraj Tiwari <tniraj7@gmail.com>
2019-12-17 17:13:28 +0900

    make course data model and pass it to CourseView inside scrolling list

--- Learn-iOS/Learn-iOS/HomeList.swift
+++ Learn-iOS/Learn-iOS/HomeList.swift
@@ -1,27 +1,78 @@
 import SwiftUI
 
+struct Course: Identifiable {
+    var id = UUID()
+    var title: String
+    var image: String
+    var color: Color
+    var shadowColor: Color
+}
+
+let coursesData = [
+    Course(
+        title: "Build an app with SwiftUI",
+        image: "Illustration1",
+        color: Color("background3"),
+        shadowColor: Color("backgroundShadow3")
+    ),
+    
+    Course(
+        title: "Design Course",
+        image: "Illustration2",
+        color: Color("background4"),
+        shadowColor: Color("backgroundShadow4")
+    )
+]
+
 struct HomeList: View {
+    
+    var courses = coursesData
+    @State var showCourses: Bool
+    
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false){
             HStack {
-                ForEach(0 ..< 3) { item in
-                    CourseView()
+                ForEach(courses) { item in
+                    Button(action: { self.showCourses.toggle() }) {
+                        CourseView(
+                            title: item.title,
+                            image: item.image,
+                            color: item.color,
+                            shadowColor: item.shadowColor
+                        )
+                         .sheet(isPresented: self.$showCourses) {
+                                ContentView()
+                        }
+                    }
                 }
             }
-        }.shadow(color: Color("backgroundShadow3"), radius: 20, x: 0, y: 20)
+        }
     }
 }
 
 struct HomeList_Previews: PreviewProvider {
     static var previews: some View {
-        HomeList()
+        HomeList(showCourses: false)
     }
 }
 
 struct CourseView: View {
+    
+    var title: String
+    var image: String
+    var color: Color
+    var shadowColor: Color
+
+    init(title: String, image: String, color: Color, shadowColor: Color) {
+        self.title = title
+        self.image = image
+        self.color = color
+        self.shadowColor = shadowColor
+    }
+    
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Build an app with SwiftUI")
+            Text(title)
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundColor(Color.white)
@@ -29,10 +80,12 @@ struct CourseView: View {
                 .lineLimit(4)
                 .frame(width: 150)
             Spacer()
-            Image("Illustration1")
+            Image(image)
+                .renderingMode(.original)
         }
-        .background(Color("background3"))
+        .background(color)
         .cornerRadius(30)
         .frame(width: 246, height: 360)
+        .shadow(color: shadowColor, radius: 20, x: 0, y: 20)
     }
 }
