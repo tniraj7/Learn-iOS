0aeef08a787837ca7b84e4f505d26164ebc06bfd
Niraj Tiwari <tniraj7@gmail.com>
2019-12-20 20:14:11 +0900

    add Update detail page

---
+++ Learn-iOS/Learn-iOS/UpdateDetail.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct UpdateDetail: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct UpdateDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateDetail()
+    }
+}
