fb34e6555e794a4a6ff05a9b4578b1b0770fae49
Niraj Tiwari <tniraj7@gmail.com>
2019-12-23 21:58:03 +0900

    Add Home view as tab item

--- Learn-iOS/Learn-iOS/TabBar.swift
+++ Learn-iOS/Learn-iOS/TabBar.swift
@@ -3,8 +3,15 @@ import SwiftUI
 struct TabBar: View {
     var body: some View {
         TabView(selection: .constant(1)) {
-        Text("Tab Content 1").tabItem { Text("Tab Label 1") }.tag(1)
-        Text("Tab Content 2").tabItem { Text("Tab Label 2") }.tag(2)
+
+            Home().tabItem {
+                VStack {
+                    Image("IconHome")
+                    Text("Home")
+                }
+            }.tag(1)
+
+
         }
     }
 }
