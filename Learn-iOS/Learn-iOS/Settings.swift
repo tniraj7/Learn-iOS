6e545aee4e20d6e55dba1d449f0a60fc946c6da1
Niraj Tiwari <tniraj7@gmail.com>
2019-12-25 18:20:03 +0900

    add settings form

---
+++ Learn-iOS/Learn-iOS/Settings.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
