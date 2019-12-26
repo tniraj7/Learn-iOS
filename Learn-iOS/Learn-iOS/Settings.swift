03c71adc52eab567244636f15e55dba1238b3775
Niraj Tiwari <tniraj7@gmail.com>
2019-12-26 18:16:03 +0900

    add picker view to form and bind it with $selection

--- Learn-iOS/Learn-iOS/Settings.swift
+++ Learn-iOS/Learn-iOS/Settings.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct Settings: View {
     @State var recieve = false
     @State var number = 1
+    @State var selection = 1
     
     var body: some View {
         NavigationView {
@@ -13,6 +14,11 @@ struct Settings: View {
                 
                 Stepper(value: $number, in: 1...10) {
                     Text("\(number) Notification\(number > 1 ? "s": "") per week")
+                }
+                
+                Picker(selection: $selection, label: Text("Favorite Course")) {
+                    Text("SwiftUI").tag(1)
+                    Text("React.js").tag(2)
                 }
             }
             .navigationBarTitle("Settings")
