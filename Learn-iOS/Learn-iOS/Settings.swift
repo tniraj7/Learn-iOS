ac40bc3d8887d71f39300fad50f64c756aeaa89e
Niraj Tiwari <tniraj7@gmail.com>
2019-12-26 17:47:51 +0900

    add stepper control to form

--- Learn-iOS/Learn-iOS/Settings.swift
+++ Learn-iOS/Learn-iOS/Settings.swift
@@ -2,12 +2,17 @@ import SwiftUI
 
 struct Settings: View {
     @State var recieve = false
+    @State var number = 1
     
     var body: some View {
         NavigationView {
             Form {
                 Toggle(isOn: $recieve) {
                     Text("Recieve notifications")
+                }
+                
+                Stepper(value: $number, in: 1...10) {
+                    Text("\(number) Notification\(number > 1 ? "s": "") per week")
                 }
             }
             .navigationBarTitle("Settings")
