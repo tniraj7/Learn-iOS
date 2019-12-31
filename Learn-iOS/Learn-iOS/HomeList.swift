474a3a1cf5bb8bd0e8eab1d4c4757e68e4de2ffb
Niraj Tiwari <tniraj7@gmail.com>
2019-12-31 23:18:54 +0900

    add more data for cards

--- Learn-iOS/Learn-iOS/HomeList.swift
+++ Learn-iOS/Learn-iOS/HomeList.swift
@@ -9,19 +9,26 @@ struct Course: Identifiable {
 }
 
 let coursesData = [
-    Course(
-        title: "Build an app with SwiftUI",
-        image: "Illustration1",
-        color: Color("background3"),
-        shadowColor: Color("backgroundShadow3")
-    ),
-    
-    Course(
-        title: "Design Course",
-        image: "Illustration2",
-        color: Color("background4"),
-        shadowColor: Color("backgroundShadow4")
-    )
+    Course(title: "Build an app with SwiftUI",
+           image: "Illustration1",
+           color: Color("background3"),
+           shadowColor: Color("backgroundShadow3")),
+    Course(title: "Design and animate your UI",
+           image: "Illustration2",
+           color: Color("background4"),
+           shadowColor: Color("backgroundShadow4")),
+    Course(title: "Swift UI Advanced",
+           image: "Illustration3",
+           color: Color("background7"),
+           shadowColor: Color(hue: 0.677, saturation: 0.701, brightness: 0.788, opacity: 0.5)),
+    Course(title: "Framer Playground",
+           image: "Illustration4",
+           color: Color("background8"),
+           shadowColor: Color(hue: 0.677, saturation: 0.701, brightness: 0.788, opacity: 0.5)),
+    Course(title: "Flutter for Designers",
+           image: "Illustration5",
+           color: Color("background9"),
+           shadowColor: Color(hue: 0.677, saturation: 0.701, brightness: 0.788, opacity: 0.5)),
 ]
 
 struct HomeList: View {
